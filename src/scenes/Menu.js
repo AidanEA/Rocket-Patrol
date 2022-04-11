171ca0eabd3d90865656a6188871ec4165ead8a6
Aidan Adams <aieadams@ucsc.edu>
2022-04-10 22:18:25 -0700

    Added sound effects

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,7 +2,12 @@ class Menu extends Phaser.Scene {
     constructor() {
       super("menuScene");
     }
-    
+    preload() {
+      // load audio
+      this.load.audio('sfx_select', './assets/mixkit-retro-game-notification-212.wav');
+      this.load.audio('sfx_explosion', './assets/amogusplosion.wav');
+      this.load.audio('sfx_rocket', './assets/mixkit-fast-small-sweep-transition-166.wav');
+    } 
     create() {
       this.add.text(20, 20, "Rocket Patrol Menu");
       this.scene.start("playScene");
